ad0ca3def6fb9af49dc27a8f5dd6be1f6e989ab2
WhenSunset <1018998632@qq.com>
2019-04-16 13:02:24 +0800

    优化例子

--- test/src/main/java/com/whensunset/test/MainActivity.java
+++ test/src/main/java/com/whensunset/test/MainActivity.java
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.whensunset.sticker.AnimationElement;
 import com.whensunset.sticker.RuleLineElementContainerView;
 import com.whensunset.sticker.Sticker;
 
@@ -22,38 +24,38 @@ public class MainActivity extends Activity {
     Sticker.initialize(this);
     mElementContainerView = findViewById(R.id.element_container_view);
     mElementContainerView.setNeedAutoUnSelect(false);
-    StaticStickerElement staticStickerElement = StaticStickerElement.getStaticStickerElementByUri(
-        500, 500, "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2335883016,884179302&fm=200&gp=0.jpg");
-    mElementContainerView.unSelectElement();
-    mElementContainerView.addSelectAndUpdateElement(staticStickerElement);
     
-//    Button addTestElement = findViewById(R.id.add_test_element);
-//    addTestElement.setOnClickListener(v -> {
-//      TestElement testElementDrawer = new TestElement(500, 500);
-//      mElementContainerView.unSelectElement();
-//      mElementContainerView.addSelectAndUpdateElement(testElementDrawer);
-//    });
-//
-//    Button addStaticElement = findViewById(R.id.add_static_element);
-//    addStaticElement.setOnClickListener(v -> {
-//
-//    });
-//
-//    Button startAnimation = findViewById(R.id.start_animation);
-//    startAnimation.setOnClickListener(v -> {
-//      AnimationElement element = (AnimationElement) mElementContainerView.getSelectElement();
-//      AnimationElement.TransformParam to = new AnimationElement.TransformParam(element);
-//      to.mRotate = 0;
-//      to.mScale = 1;
-//      to.mMoveY += 500;
-//      to.mMoveX += 500;
-//      element.startElementAnimation(to);
-//    });
-//
-//    Button restoreAnimation = findViewById(R.id.restore_animation);
-//    restoreAnimation.setOnClickListener(v -> {
-//      AnimationElement element = (AnimationElement) mElementContainerView.getSelectElement();
-//      element.restoreToBeforeAnimation();
-//    });
+    
+    Button addTestElement = findViewById(R.id.add_test_element);
+    addTestElement.setOnClickListener(v -> {
+      TestElement testElementDrawer = new TestElement(500, 500);
+      mElementContainerView.unSelectElement();
+      mElementContainerView.addSelectAndUpdateElement(testElementDrawer);
+    });
+    
+    Button addStaticElement = findViewById(R.id.add_static_element);
+    addStaticElement.setOnClickListener(v -> {
+      StaticStickerElement staticStickerElement = StaticStickerElement.getStaticStickerElementByUri(
+          500, 500, "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2335883016,884179302&fm=200&gp=0.jpg");
+      mElementContainerView.unSelectElement();
+      mElementContainerView.addSelectAndUpdateElement(staticStickerElement);
+    });
+    
+    Button startAnimation = findViewById(R.id.start_animation);
+    startAnimation.setOnClickListener(v -> {
+      AnimationElement element = (AnimationElement) mElementContainerView.getSelectElement();
+      AnimationElement.TransformParam to = new AnimationElement.TransformParam(element);
+      to.mRotate = 0;
+      to.mScale = 1;
+      to.mMoveY += 500;
+      to.mMoveX += 500;
+      element.startElementAnimation(to);
+    });
+    
+    Button restoreAnimation = findViewById(R.id.restore_animation);
+    restoreAnimation.setOnClickListener(v -> {
+      AnimationElement element = (AnimationElement) mElementContainerView.getSelectElement();
+      element.restoreToBeforeAnimation();
+    });
   }
 }
